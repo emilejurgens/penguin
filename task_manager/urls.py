87c34685009bb175ce5948b45ad9f5bdb2e16e4c
Adil Mazhitov <k21117945@kcl.ac.uk>
2023-12-06 15:04:09 +0000

    Add user activity log

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('password/', views.PasswordView.as_view(), name='password'),
     path('profile/', views.ProfileUpdateView.as_view(), name='profile'),
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
-    path('task_view/', views.task_view, name = 'task_view'),
 ]
