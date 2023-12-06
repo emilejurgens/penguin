e3accc5843bdd3ac5910aabd3998a22f5dfab902
Emile Jurgens <emile.jurgens@kcl.ac.uk>
2023-12-06 17:32:30 +0000

    added admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,38 +1,32 @@
-# from django.contrib import admin
-# from .models import User, Team, Task
+from django.contrib import admin
+from .models import User, Team, Task
 
-# @admin.register(User)
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = [
-#         'username', 'first_name', 'last_name', 'email', 'is_active',
-#     ]
-
-# @admin.register(Team)
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = [
-#         'name'
-#     ]
-# # Register your models here.
+@admin.register(Team)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'name'
+    ]
+# Register your models here.
 
 
-# @admin.register(User)
-# class UserAdmin(admin.ModelAdmin):
-#     """Configuration of the admin interface for users."""
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for users."""
 
-#     list_display = [
-#         'username', 'first_name', 'last_name', 'email', 'is_active',
-#     ]
+    list_display = [
+        'username', 'first_name', 'last_name', 'email', 'is_active',
+    ]
 
-# @admin.register(Team)
-# class TeamAdmin(admin.ModelAdmin):
-#     """Configuration of the admin interface for teams."""
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for teams."""
 
-#     list_display = [
-#         'name',
-#     ]
+    list_display = [
+        'name',
+    ]
 
-# # @admin.register(Task)
-# # class TaskAdmin(admin.ModelAdmin):
-# #     """Configuration of the admin interface for tasks."""
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for tasks."""
 
-# #     pass
+    pass
