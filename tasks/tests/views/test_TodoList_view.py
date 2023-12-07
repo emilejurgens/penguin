1fa593f7ddd2cccb68b8651445b4ae483e267670
Sara Alaqeel <sara.f.alaqeel@gmail.com>
2023-12-07 20:03:09 +0000

    test for todo

--- tasks/tests/views/test_TodoList_view.py
+++ tasks/tests/views/test_TodoList_view.py
@@ -8,14 +8,14 @@ class TodoListViewTest(TestCase):
 
     databases = "__all__"
     def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/todos/')
+        response = self.client.get('/todo/')
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('todo_list'))
+        response = self.client.get(reverse('todo'))
         self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('todo_list'))
+        response = self.client.get(reverse('todo'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'todo_list.html')
+        self.assertTemplateUsed(response, 'todo.html')
