edf6ed0646b0f451de4c4cd9149a29a4355243d0
Kamila Shadieva <kamila.shadieva@kcl.ac.uk>
2023-12-13 17:08:56 +0000

    Fix edit team

--- tasks/views.py
+++ tasks/views.py
@@ -243,9 +243,6 @@ class EditTeamView(LoginRequiredMixin, FormView):
         """Return redirect URL after successful update."""
         messages.add_message(self.request, messages.SUCCESS, "Team updated!")
         return reverse(settings.REDIRECT_URL_WHEN_LOGGED_IN)
-    
-    def get(self, request):
-        return render(request, 'edit_team.html')
 
 
 class TaskView(TaskForm):
